7cf6e166ce7afcaa98ea031ae9b2afe90f449fcb
qiuxinol <1557482811@qq.com>
2020-04-16 18:32:20 +0800

    第一个

---
+++ app/src/main/java/com/zhengtai/git_text02/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zhengtai.git_text02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
